757c88593f98dd0a9912c0cae4e1ca45c02a3a88
sp <reza.ghasemi8@gmail.com>
2021-06-19 13:37:33 +0430

    second commit

--- UnitTest/BankAccount.cs
+++ UnitTest/BankAccount.cs
@@ -10,6 +10,7 @@ using NUnit.Framework;
 
 namespace UnitTest
 {
+
     public interface ILog
     {
         void WriteMessage(string msg);
@@ -140,6 +141,49 @@ namespace UnitTest
             Assert.That(ba.Balance,Is.EqualTo(150));
         }
 
-       
+        [Test]
+        public void Test()
+        {
+            // var _actualFuel = 2 + 2;
+            //   Assert.Inconclusive("The Result is not Set");
+            // Assert.Warn("The Result is not Set");
+            //   Assert.AreEqual(a,4);
+            // Assert.That(_actualFuel, Is.EqualTo(4));
+
+            //Assert.That(28, Is.EqualTo(_actualFuel)); // Tests whether the specified values are equal. 
+            //Assert.That(28, Is.Not.EqualTo(_actualFuel)); // Tests whether the specified values are unequal. Same as AreEqual for numeric values.
+            //Assert.That(_expectedRocket, Is.SameAs(_actualRocket)); // Tests whether the specified objects both refer to the same object
+            //Assert.That(_expectedRocket, Is.Not.SameAs(_actualRocket)); // Tests whether the specified objects refer to different objects
+            //Assert.That(_isThereEnoughFuel, Is.True); // Tests whether the specified condition is true
+            //Assert.That(_isThereEnoughFuel, Is.False); // Tests whether the specified condition is false
+            //Assert.That(_actualRocket, Is.Null); // Tests whether the specified object is null
+            //Assert.That(_actualRocket, Is.Not.Null); // Tests whether the specified object is non-null
+            //Assert.That(_actualRocket, Is.InstanceOf<Falcon9Rocket>()); // Tests whether the specified object is an instance of the expected type
+            //Assert.That(_actualRocket, Is.Not.InstanceOf<Falcon9Rocket>()); // Tests whether the specified object is not an instance of type
+            //Assert.That(_actualFuel, Is.GreaterThan(20)); // Tests whether the specified object greater than the specified value
+            //Assert.That(28, Is.EqualTo(_actualFuel).Within(0.50));
+            //// Tests whether the specified values are nearly equal within the specified tolerance.
+            //Assert.That(28, Is.EqualTo(_actualFuel).Within(2).Percent);
+            //// Tests whether the specified values are nearly equal within the specified % tolerance.
+            //Assert.That(_actualRocketParts, Has.Exactly(10).Items);
+            //// Tests whether the specified collection has exactly the stated number of items in it.
+            //Assert.That(_actualRocketParts, Is.Unique);
+            //// Tests whether the items in the specified collections are unique.
+            //Assert.That(_actualRocketParts, Does.Contain(_expectedRocketPart));
+            //// Tests whether a given items is present in the specified list of items.
+            //Assert.That(_actualRocketParts, Has.Exactly(1).Matches<RocketPart>(part => part.Name == "Door" && part.Height == "200"));
+            //// Tests whether the specified collection has exactly the stated item in it.
+
+
+            //StringAssert.AreEqualIgnoringCase(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified strings are equal ignoring their casing
+            //StringAssert.Contains(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string contains the specified substring
+            //StringAssert.DoesNotContain(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string doesn't contain the specified substring
+            //StringAssert.StartsWith(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string begins with the specified substring
+            //StringAssert.StartsWith(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string begins with the specified substring
+            //StringAssert.IsMatch("(281)388-0388", @"(?d{3})?-? *d{3}-? *-?d{4}"); // Tests whether the specified string matches a regular expression
+            //StringAssert.DoesNotMatch("281)388-0388", @"(?d{3})?-? *d{3}-? *-?d{4}"); // Tests whether the specified string does not match a regular expression
+
+        }
+
     }
 }
